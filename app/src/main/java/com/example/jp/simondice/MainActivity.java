c1fc6e630f9cb07f59b578693521045c21188fae
19julio94 <juliopatriciodasilva@gmail.com>
2016-11-06 23:59:28 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jp/simondice/MainActivity.java
@@ -0,0 +1,234 @@
+package com.example.jp.simondice;
+
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button iniciar;
+    Handler retardo1=new Handler();
+    Handler retardo2=new Handler();
+    Button[]botones;
+    int numeros[];
+    int ordenJugador[];
+    int botonesPulsados=0;
+    private int tiempoEncendido=1000;
+    private int tiempoApagado=1500;
+    boolean enabledPlay = false;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ordenJugador=new int[4];
+        numeros=new int[4];
+        botones=new Button[4];
+
+        botones[0] = (Button) findViewById(R.id.bazul);
+        botones[1] = (Button) findViewById(R.id.brojo);
+        botones[2] = (Button) findViewById(R.id.bverde);
+        botones[3] = (Button) findViewById(R.id.bamarillo);
+
+    }
+
+
+    public void clickar(View v) {
+        int indice;
+        int id = v.getId();
+        final Button b = (Button)v;
+
+        if (id == R.id.bazul) {
+
+            v.setBackgroundResource(R.color.azulfuerte);
+            indice=0;
+
+        } else if (id == R.id.brojo) {
+
+
+            v.setBackgroundResource(R.color.rojofuerte);
+            indice=1;
+
+        } else if (id == R.id.bverde) {
+
+
+            v.setBackgroundResource(R.color.verdefuerte);
+            indice=2;
+
+        } else {
+
+            v.setBackgroundResource(R.color.amarillofuerte);
+            indice=3;
+
+        }
+        Handler handler=new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                resetear(b.getId());
+            }
+        },500);
+
+        if(enabledPlay){
+
+            ordenJugador[botonesPulsados]=indice;
+            botonesPulsados++;
+
+            if(botonesPulsados==4){
+
+                check();
+
+            }
+
+
+        }
+
+    }
+
+
+    public void iniciar(View v) {
+        enabledPlay=true;
+        for (int i = 0; i < numeros.length; i++) {
+
+            numeros[i] = (int) (Math.random() * 4);
+           final Button b=botones[numeros[i]];
+            System.out.println(numeros[i]);
+
+            if(b.getId()==R.id.bazul){
+                retardo1.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        b.setBackgroundResource(R.color.azulfuerte);
+                    }
+                },tiempoEncendido);
+
+
+
+            }else if(b.getId()==R.id.brojo){
+
+                retardo1.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        b.setBackgroundResource(R.color.rojofuerte);
+                    }
+                },tiempoEncendido);
+            }else if(b.getId()==R.id.bverde){
+
+                retardo1.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        b.setBackgroundResource(R.color.verdefuerte);
+                    }
+                },tiempoEncendido);
+            }else {
+
+                retardo1.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        b.setBackgroundResource(R.color.amarillofuerte);
+                    }
+                },tiempoEncendido);
+            }
+
+            final Button boton= b;
+
+            retardo2.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                   resetear(boton.getId());
+                }
+            },tiempoApagado);
+
+            tiempoEncendido+=1500;
+            tiempoApagado+=1000;
+
+
+        }
+        tiempoEncendido=1500;
+        tiempoApagado=1000;
+
+    }
+
+    public void resetear(int id) {
+
+
+
+        if (id == R.id.bazul) {
+
+           botones[0].setBackgroundResource(R.color.azul);
+
+
+        } else if (id == R.id.brojo) {
+
+
+            botones[1].setBackgroundResource(R.color.rojo);
+
+
+        } else if (id == R.id.bverde) {
+
+
+            botones[2].setBackgroundResource(R.color.verde);
+
+
+        } else {
+
+            botones[3].setBackgroundResource(R.color.amarillo);
+
+
+        }
+
+
+    }
+
+public void check(){
+
+    int acertados=0;
+
+    for(int i=0;i<numeros.length;i++){
+
+        if(numeros[i]!=ordenJugador[i]){
+
+
+            Toast.makeText(this, "Has perdido", Toast.LENGTH_SHORT).show();
+        }else{
+
+            acertados++;
+
+        }
+
+
+        if(acertados==4){
+            Toast.makeText(this, "Has ganado", Toast.LENGTH_SHORT).show();
+        }
+        enabledPlay=false;
+        botonesPulsados=0;
+        numeros=new int[4];
+        ordenJugador=new int[4];
+
+    }
+
+
+
+
+}
+
+
+}
+
+
+
+
+
+
+
